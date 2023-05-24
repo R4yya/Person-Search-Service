6500bf204508fe6150c8bb7fd39491c203b5d475
R4yya <121268707+R4yya@users.noreply.github.com>
2023-05-24 21:39:54 +0300

    Delete activate_venv.py

--- project_scripts/activate_venv.py
+++
@@ -1,9 +0,0 @@
-import os
-
-venv_path = "presonsearchenv"
-activate_this = os.path.join(venv_path, "bin", "activate_this.py")
-
-
-# Активация виртуального окружения
-def activate_myvenv():
-    exec(open(activate_this).read(), {'__file__': activate_this})
