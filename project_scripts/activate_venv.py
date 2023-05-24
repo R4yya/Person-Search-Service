460b50033254cadac7cfae04cd1d695fff792999
R4yya <121268707+R4yya@users.noreply.github.com>
2023-05-24 19:55:20 +0300

    Update activate_venv.py

--- project_scripts/activate_venv.py
+++ project_scripts/activate_venv.py
@@ -3,5 +3,7 @@ import os
 venv_path = "presonsearchenv"
 activate_this = os.path.join(venv_path, "bin", "activate_this.py")
 
+
 # Активация виртуального окружения
-exec(open(activate_this).read(), {'__file__': activate_this})
+def activate_myvenv():
+    exec(open(activate_this).read(), {'__file__': activate_this})
