1612ea8bdffde5d886ca9b74b3636ccc2f4fb712
R4yya <121268707+R4yya@users.noreply.github.com>
2023-05-26 19:26:32 +0300

    Update convert_image_to_bytes.py

--- project_scripts/convert_image_to_bytes.py
+++ project_scripts/convert_image_to_bytes.py
@@ -3,6 +3,23 @@ from os.path import dirname, splitext
 from os.path import join as os_join
 from cv2 import imread, imencode
 
+
+def convert_image_to_bytes(image_path):
+    # Загрузка изображения с помощью OpenCV
+    image_cv2 = imread(image_path)
+
+    # Преобразование изображения в байтовый массив
+    image_bytes = imencode('.jpg', image_cv2)[1].tobytes()
+
+    return image_bytes
+
+
+def write_bytes_to_file(output_path, image_bytes):
+    # Запись байтов в файл
+    with open(output_path, 'wb') as file:
+        file.write(image_bytes)
+
+
 # Абсолютный путь проекта
 project_path = '\\'.join(dirname(__file__).split('\\')[:-1])
 
@@ -14,11 +31,8 @@ for image in images_list:
     # Полный путь к изображению
     image_path = os_join(f'{project_path}\\img', image)
 
-    # Загрузка изображения с помощью OpenCV
-    image_cv2 = imread(image_path)
-
     # Преобразование изображения в байтовый массив
-    image_bytes = (imencode('.jpg', image_cv2)[1].tobytes())
+    image_bytes = convert_image_to_bytes(image_path)
 
     # Имя выходного файла
     output_file = f'{splitext(image)[0]}.bin'
@@ -27,5 +41,4 @@ for image in images_list:
     output_path = os_join(f'{project_path}\\img_bytes', output_file)
 
     # Запись байтов в файл
-    with open(output_path, 'wb') as file:
-        file.write(image_bytes)
+    write_bytes_to_file(output_path, image_bytes)
