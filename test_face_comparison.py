98f4080485b8527cbfb5a9a725778c66e51eecf7
R4yya <121268707+R4yya@users.noreply.github.com>
2023-05-31 16:51:05 +0300

    Update test_face_comparison.py

--- test_face_comparison.py
+++ test_face_comparison.py
@@ -10,16 +10,20 @@ project_path = get_project_path()
 # image2 = face_recognition.load_image_file(f'{project_path}\\img\\test_durov_1.jpg')
 
 # Загрузка изображений из bin
-image1 = face_recognition.load_image_file(f'{project_path}\\img_bytes\\test_durov_1.bin')
-image2 = face_recognition.load_image_file(f'{project_path}\\img_bytes\\durov.bin')
+image1 = face_recognition.load_image_file(
+    f'{project_path}\\img_bytes\\test_durov_1.bin')
+image2 = face_recognition.load_image_file(
+    f'{project_path}\\img_bytes\\durov.bin')
 
 # Поиск лиц на изображениях
 face_locations1 = face_recognition.face_locations(image1)
 face_locations2 = face_recognition.face_locations(image2)
 
 # Получение face embeddings (признаки лиц)
-face_embeddings1 = np.array(face_recognition.face_encodings(image1, face_locations1))
-face_embeddings2 = np.array(face_recognition.face_encodings(image2, face_locations2))
+face_embeddings1 = np.array(
+    face_recognition.face_encodings(image1, face_locations1))
+face_embeddings2 = np.array(
+    face_recognition.face_encodings(image2, face_locations2))
 
 # Сравнение face embeddings
 results = face_recognition.compare_faces(face_embeddings1, face_embeddings2)
