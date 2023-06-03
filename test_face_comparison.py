54a9a0eb1e6c6049e7ac3e59cf21029b47bc1d50
R4yya <121268707+R4yya@users.noreply.github.com>
2023-06-03 03:41:07 +0300

    Update test_face_comparison.py

--- test_face_comparison.py
+++ test_face_comparison.py
@@ -1,7 +1,7 @@
 import face_recognition
 import numpy as np
-from convert_image_to_bytes import get_project_path
 from timeit import timeit
+from convert_image_to_bytes import get_project_path
 
 
 def load_images(image_format='.jpg'):
@@ -10,9 +10,9 @@ def load_images(image_format='.jpg'):
 
     # Load image
     image_1 = face_recognition.load_image_file(
-        f'{project_path}\\img\\test_me_2{image_format}')
+        f'{project_path}\\img\\durov{image_format}')
     image_2 = face_recognition.load_image_file(
-        f'{project_path}\\img\\test_me_3{image_format}')
+        f'{project_path}\\img\\gates{image_format}')
 
     return image_1, image_2
 
@@ -42,7 +42,7 @@ def compare_faces(face_encoding_1, face_encoding_2):
 def get_face_distances(face_encoding_1, face_encoding_2):
     # Calculate the distance between faces
     face_distances = face_recognition.face_distance(
-        [face_encoding_1], face_encoding_2)[0]
+        [face_encoding_1], face_encoding_2)
 
     return face_distances
 
@@ -51,21 +51,34 @@ def get_median(face_distances):
     # Calculate median of face_distances array
     median = np.median(face_distances)
 
-    return (1 - median) * 100
+    return median
 
 
 def get_mean(face_distances):
     # Calculate arithmetic mean
     mean = np.mean(face_distances)
 
-    return (1 - mean) * 100
+    return mean
 
 
 def get_standard_deviation(face_distances):
     # Calculate standard deviation
     standard_deviation = np.std(face_distances)
 
-    return (1 - standard_deviation) * 100
+    return standard_deviation
+
+
+def get_face_recognition_accuracy(face_distance, face_match_threshold=0.6):
+    if face_distance > face_match_threshold:
+        match_threshold_range = (1.0 - face_match_threshold)
+        linear_val = (1.0 - face_distance) / (match_threshold_range * 2.0)
+
+        return linear_val
+    else:
+        match_threshold_range = face_match_threshold
+        linear_val = 1.0 - (face_distance / (match_threshold_range * 2.0))
+
+        return linear_val + ((1.0 - linear_val) * np.power((linear_val - 0.5) * 2, 0.2))
 
 
 if __name__ == '__main__':
@@ -75,11 +88,15 @@ if __name__ == '__main__':
     face_distances = get_face_distances(face_encoding_1, face_encoding_2)
 
     # Print comparison result
-    # comparison_result = compare_faces(face_encoding_1, face_encoding_2)
-    # if any(comparison_result):
-    #     print("The faces in the images converge")
-    # else:
-    #     print("The faces in the images do not converge")
+    comparison_result = compare_faces(face_encoding_1, face_encoding_2)
+
+    if any(comparison_result):
+        print("The faces in the images converge\n")
+    else:
+        print("The faces in the images do not converge\n")
+
+    # Print face distances
+    print(f'{face_distances}\n')
 
     # Testing aggregation methods and their execution time
     # 1. Median
@@ -88,7 +105,7 @@ if __name__ == '__main__':
     execution_time = timeit(lambda: get_median(face_distances), number=20)
 
     print('Median')
-    print(f'Similarity is {similarity_score_median:.2f}%')
+    print(f'Similarity is {similarity_score_median:.5f}')
     print(f'Execution time: {execution_time} seconds\n')
 
     # 2. Arithmetic mean
@@ -97,7 +114,7 @@ if __name__ == '__main__':
     execution_time = timeit(lambda: get_mean(face_distances), number=20)
 
     print('Arithmetic mean')
-    print(f'Similarity is {similarity_score_mean:.2f}%')
+    print(f'Similarity is {similarity_score_mean:.5f}')
     print(f'Execution time: {execution_time} seconds\n')
 
     # 3. Standard deviation
@@ -107,5 +124,18 @@ if __name__ == '__main__':
         lambda: get_standard_deviation(face_distances), number=20)
 
     print('Standard deviation')
-    print(f'Similarity is {similarity_score_sd}%')
+    print(f'Similarity is {similarity_score_sd:.5f}')
+    print(f'Execution time: {execution_time} seconds\n')
+
+    # 4. face_recognition wikis solution
+    # https://github.com/ageitgey/face_recognition/wiki/Calculating-Accuracy-as-a-Percentage
+    # which is quite similar to face_recognition.compare_faces but returns
+    # a sigle value
+    similarity_score_fra = get_face_recognition_accuracy(get_median(face_distances))
+
+    execution_time = timeit(
+        lambda: get_face_recognition_accuracy(get_median(face_distances)), number=20)
+
+    print('face_recognition wikis solution')
+    print(f'Similarity is {similarity_score_fra}')
     print(f'Execution time: {execution_time} seconds')
