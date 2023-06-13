2e01dcd21c9d9b7d4f89beda77e88fb7d765949e
R4yya <121268707+R4yya@users.noreply.github.com>
2023-06-13 23:16:13 +0300

    Update test_face_comparison.py

--- test_face_comparison.py
+++ test_face_comparison.py
@@ -1,6 +1,5 @@
 import face_recognition
 import numpy as np
-from timeit import timeit
 from convert_image_to_bytes import get_project_path
 
 
@@ -12,7 +11,7 @@ def load_images(image_format='.jpg'):
     image_1 = face_recognition.load_image_file(
         f'{project_path}\\img\\durov{image_format}')
     image_2 = face_recognition.load_image_file(
-        f'{project_path}\\img\\gates{image_format}')
+        f'{project_path}\\img\\test_durov_1{image_format}')
 
     return image_1, image_2
 
@@ -69,16 +68,43 @@ def get_standard_deviation(face_distances):
 
 
 def get_face_recognition_accuracy(face_distance, face_match_threshold=0.6):
-    if face_distance > face_match_threshold:
-        match_threshold_range = (1.0 - face_match_threshold)
-        linear_val = (1.0 - face_distance) / (match_threshold_range * 2.0)
+    # face_recognition wikis modified solution
+    # https://github.com/ageitgey/face_recognition/wiki/Calculating-Accuracy-as-a-Percentage
+    # which is quite similar to face_recognition.compare_faces but returns a sigle value
+    confidences = []
+    for distance in np.nditer(face_distance):
+        if distance > face_match_threshold:
+            match_threshold_range = (1.0 - face_match_threshold)
+            linear_val = (1.0 - distance) / (match_threshold_range * 2.0)
+            confidence = linear_val
 
-        return linear_val
-    else:
-        match_threshold_range = face_match_threshold
-        linear_val = 1.0 - (face_distance / (match_threshold_range * 2.0))
+            return linear_val
+        else:
+            match_threshold_range = face_match_threshold
+            linear_val = 1.0 - (distance / (match_threshold_range * 2.0))
+            confidence = linear_val + \
+                ((1.0 - linear_val) * ((linear_val - 0.5) * 2) ** 0.2)
+        confidences.append(confidence)
+    return np.array(confidences)
 
-        return linear_val + ((1.0 - linear_val) * np.power((linear_val - 0.5) * 2, 0.2))
+
+def match_percentage(face_distance):
+    face_match_percentage = (1 - face_distances) * 100
+    for i, face_distance in enumerate(face_distances):
+        print(f'The test image has a distance of {face_distance:.2} from known image #{i}')
+
+        print(f'- comparing with a tolerance of 0.6? {face_distance < 0.6}')
+
+        print(np.round(face_match_percentage, 4))  # upto 4 decimal places
+
+
+def face_distance_to_similarity(face_distance, face_match_threshold=0.6):
+    similarity = 1 - (face_distance - face_match_threshold) / \
+        (1 - face_match_threshold)
+    similarity = np.clip(similarity, 0, 1)  # limit in range [0, 1]
+    similarity_percent = similarity * 100
+
+    return similarity_percent
 
 
 if __name__ == '__main__':
@@ -91,51 +117,19 @@ if __name__ == '__main__':
     comparison_result = compare_faces(face_encoding_1, face_encoding_2)
 
     if any(comparison_result):
-        print("The faces in the images converge\n")
+        print('The faces in the images converge\n')
     else:
-        print("The faces in the images do not converge\n")
+        print('The faces in the images do not converge\n')
 
     # Print face distances
     print(f'{face_distances}\n')
 
-    # Testing aggregation methods and their execution time
-    # 1. Median
-    similarity_score_median = get_median(face_distances)
+    similarities = get_face_recognition_accuracy(face_distances)
 
-    execution_time = timeit(lambda: get_median(face_distances), number=20)
+    print(f'{similarities}\n')
 
-    print('Median')
-    print(f'Similarity is {similarity_score_median:.5f}')
-    print(f'Execution time: {execution_time} seconds\n')
-
-    # 2. Arithmetic mean
-    similarity_score_mean = get_mean(face_distances)
-
-    execution_time = timeit(lambda: get_mean(face_distances), number=20)
-
-    print('Arithmetic mean')
-    print(f'Similarity is {similarity_score_mean:.5f}')
-    print(f'Execution time: {execution_time} seconds\n')
-
-    # 3. Standard deviation
-    similarity_score_sd = get_standard_deviation(face_distances)
-
-    execution_time = timeit(
-        lambda: get_standard_deviation(face_distances), number=20)
-
-    print('Standard deviation')
-    print(f'Similarity is {similarity_score_sd:.5f}')
-    print(f'Execution time: {execution_time} seconds\n')
-
-    # 4. face_recognition wikis solution
-    # https://github.com/ageitgey/face_recognition/wiki/Calculating-Accuracy-as-a-Percentage
-    # which is quite similar to face_recognition.compare_faces but returns
-    # a sigle value
-    similarity_score_fra = get_face_recognition_accuracy(get_median(face_distances))
-
-    execution_time = timeit(
-        lambda: get_face_recognition_accuracy(get_median(face_distances)), number=20)
+    average_similarity = np.mean(similarities)
+    similarity_percent = average_similarity * 100
 
     print('face_recognition wikis solution')
-    print(f'Similarity is {similarity_score_fra}')
-    print(f'Execution time: {execution_time} seconds')
+    print(f'Similarity is {similarity_percent}%')
