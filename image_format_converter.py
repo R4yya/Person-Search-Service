e03273eacecdb401d7b9b78fc01b47ebbd3410b6
R4yya <121268707+R4yya@users.noreply.github.com>
2023-06-15 05:54:22 +0300

    Update image_format_converter.py

--- image_format_converter.py
+++ image_format_converter.py
@@ -1,14 +1,13 @@
 from PIL import Image
 from convert_image_to_bytes import get_project_path
 
-# Аbsolute path of the project
-project_path = get_project_path()
 
-# File name
-file_name = input()
+def convert_image(file_name):
+    # Аbsolute path of the project
+    project_path = get_project_path()
 
-# Open image in .png format
-image = Image.open(f'{get_project_path}\\img\\{file_name}.png')
+    # Open image in .png format
+    image = Image.open(f'{project_path}\\img\\{file_name}.png')
 
-# Save image in .jpg format
-image.save(f'{get_project_path}\\img\\{file_name}.jpg')
+    # Save image in .jpg format
+    image.save(f'{project_path}\\img\\{file_name}.jpg')
