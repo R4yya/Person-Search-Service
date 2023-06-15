309f75d770f3c7111bb57201ff1ef04b0613c1d5
R4yya <121268707+R4yya@users.noreply.github.com>
2023-06-15 06:18:10 +0300

    Update faceDataModel.py

--- src/models/faceDataModel.py
+++ src/models/faceDataModel.py
@@ -7,10 +7,8 @@ from sqlalchemy.orm import relationship
 class FaceDataModel(BaseModel):
     __tablename__ = 'face_data'
 
-    person_id = Column(Integer, ForeignKey('persons.identifier'),
-                       nullable=False)
-    encoding = Column(ARRAY(REAL, dimensions=2), nullable=False)
-    location = Column(ARRAY(Integer, dimensions=2, as_tuple=True),
-                      nullable=False)
+    person_id = Column(Integer, ForeignKey('persons.identifier'), nullable=False)
+    encoding = Column(ARRAY(REAL), nullable=False)
+    location = Column(ARRAY(Integer, as_tuple=True), nullable=False)
 
     person = relationship('PersonsModel', back_populates='face_encodings')
