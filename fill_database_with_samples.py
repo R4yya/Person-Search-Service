37ec9daccf9dc4f43aae4af890037968f6b05987
R4yya <121268707+R4yya@users.noreply.github.com>
2023-06-15 05:55:12 +0300

    Update fill_database_with_samples.py

--- fill_database_with_samples.py
+++ fill_database_with_samples.py
@@ -1,60 +1,71 @@
-from src.databaseSession import DatabaseSession
-from src.personsModel import PersonsModel
+from src.models.databaseSession import DatabaseSession
+from src.models.personsModel import PersonsModel
 from convert_image_to_bytes import get_bytes_from_file
 
 
-# Set URI
-username = input('username: ')
-password = input('password: ')
-uri = f'postgresql://{username}:{password}@localhost:5432/person_search'
+def initialize_database_session():
+    # Get URI
+    username = input('username: ')
+    password = input('password: ')
+    uri = f'postgresql://{username}:{password}@localhost:5432/person_search'
 
-# Create an instance of the DatabaseSession
-db_session = DatabaseSession(uri)
+    # Create an instance of the DatabaseSession
+    db_session = DatabaseSession(uri)
 
-# Get session from DatabaseSession
-session = db_session.get_session()
+    # Get session from DatabaseSession
+    session = db_session.get_session()
 
-# Create data samples to fill table
-samples = [
-    {
-        'first_name': 'Pavel',
-        'last_name': 'Durov',
-        'patronymic': 'Valeryevich',
-        'birthdate': '1994-10-10',
-        'country': 'Russia',
-        'phone_num': '+79155590507',
-        'email': 'durov@mail.com',
-        'photo': get_bytes_from_file('durov.bin')
-    },
-    {
-        'first_name': 'Gates',
-        'last_name': 'William',
-        'patronymic': None,
-        'birthdate': '1955-10-28',
-        'country': 'USA',
-        'phone_num': '+18332691066',
-        'email': 'gates@mail.com',
-        'photo': get_bytes_from_file('gates.bin')
-    },
-    {
-        'first_name': 'Mark',
-        'last_name': 'Zuckerberg',
-        'patronymic': None,
-        'birthdate': '1984-05-14',
-        'country': 'USA',
-        'phone_num': '+18332626819',
-        'email': 'zuckerberg@mail.com',
-        'photo': get_bytes_from_file('zuckerberg.bin')
-    },
-]
+    return session
 
-# Fill table using model and session
-for sample in samples:
-    person = PersonsModel(**sample)
-    session.add(person)
 
-# Perform a commit to save the data in database
-session.commit()
+def fill_persons_table(samples):
+    # Fill table using model and session
+    for sample in samples:
+        person = PersonsModel(**sample)
+        session.add(person)
 
-# End session
-session.close()
+    # Perform a commit to save the data in database
+    session.commit()
+
+    # End session
+    session.close()
+
+
+if __name__ == '__main__':
+    session = initialize_database_session()
+
+    # Create data samples to fill table
+    samples = [
+        {
+            'first_name': 'Pavel',
+            'last_name': 'Durov',
+            'patronymic': 'Valeryevich',
+            'birthdate': '1994-10-10',
+            'country': 'Russia',
+            'phone_num': '+79155590507',
+            'email': 'durov@mail.com',
+            'photo': get_bytes_from_file('durov.bin')
+        },
+        {
+            'first_name': 'Gates',
+            'last_name': 'William',
+            'patronymic': None,
+            'birthdate': '1955-10-28',
+            'country': 'USA',
+            'phone_num': '+18332691066',
+            'email': 'gates@mail.com',
+            'photo': get_bytes_from_file('gates.bin')
+        },
+        {
+            'first_name': 'Mark',
+            'last_name': 'Zuckerberg',
+            'patronymic': None,
+            'birthdate': '1984-05-14',
+            'country': 'USA',
+            'phone_num': '+18332626819',
+            'email': 'zuckerberg@mail.com',
+            'photo': get_bytes_from_file('zuckerberg.bin')
+        },
+    ]
+
+    fill_persons_table(samples)
