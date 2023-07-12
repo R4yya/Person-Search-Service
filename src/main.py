0423bab4d55f3b6b67e04afe0951644c342eb2c4
R4yya <121268707+R4yya@users.noreply.github.com>
2023-07-12 12:31:33 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ from PyQt6 import QtWidgets
 from os.path import join as os_join, dirname
 import sys
 from ui.dbConnectionWindow import DbConnectionWindow
-from db_operator.databaseOperator import DatabaseOperator
-from models.tablesModels import PersonsModel
+from db_operator.databaseSession import DatabaseSession
+from models.tablesModels import PersonsModel, FaceDataModel
+from photo_handler.photoHandler import PhotoHandler
 
 
 def update_sys_path():
 
-    folders = ['db_operator', 'models', 'saved_data_json', 'ui', 'utils']
+    folders = ['db_operator', 'models', 'photo_handler',
+               'saved_data_json', 'ui', 'utils']
 
     for folder in folders:
         fpath = os_join('\\'.join(dirname(__file__).split('\\')[:-1]), folder)
@@ -21,7 +23,10 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
     db_connection_window = DbConnectionWindow(
-        DatabaseOperator(), PersonsModel)
+        DatabaseSession(),
+        PersonsModel,
+        FaceDataModel,
+        PhotoHandler())
     db_connection_window.show()
 
     sys.exit(app.exec())
