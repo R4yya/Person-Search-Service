5bd4ab1c5e3156103d41d9ea8824d3d5626db30e
R4yya <121268707+R4yya@users.noreply.github.com>
2023-07-12 12:30:51 +0300

    Create databaseSession.py

---
+++ src/db_operator/databaseSession.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class DatabaseSession(object):
+    def __init__(self):
+        pass
+
+    def setup_session(self, db_uri):
+        self.engine = create_engine(db_uri)
+        self.session = sessionmaker(bind=self.engine)
+
+    def get_session(self):
+        return self.session()
